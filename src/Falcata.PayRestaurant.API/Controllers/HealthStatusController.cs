b509b1b7f1a540b11158206e95800b4ea45693c3
Fernando Alvarez <fernando.alvarez@wizeline.com>
2023-07-29 11:56:26 -0600

    [Task-30] Update HealthStatusController to use UserRepository

--- src/Falcata.PayRestaurant.API/Controllers/HealthStatusController.cs
+++ src/Falcata.PayRestaurant.API/Controllers/HealthStatusController.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Falcata.PayRestaurant.Application.Interfaces.Repositories.Users;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,18 +9,34 @@ namespace Falcata.PayRestaurant.API.Controllers;
 public class HealthStatusController: BaseController
 {
     private readonly IMediator _mediator;
+    private readonly IUserQueryRepository _userQueryRepository;
 
-    public HealthStatusController(IMediator mediator)
+    public HealthStatusController(IMediator mediator, IUserQueryRepository userQueryRepository)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _userQueryRepository = userQueryRepository ?? throw new ArgumentNullException(nameof(userQueryRepository));
     }
 
     [HttpGet("/")]
-    public async Task<string> ServiceHealthAsync()
+    public async Task<string> ServiceHealthAsync(CancellationToken cancellationToken)
     {
         StringBuilder result = new StringBuilder();
 
         result.AppendLine($"Mediator active: {_mediator is not null}");
+        
+        try
+        {
+            var userList = await _userQueryRepository.UserListAsync(cancellationToken);
+            result.AppendLine("Users:");
+            foreach (var user in userList)
+                result.AppendLine($"{user.Email}");
+            if(userList is { Count: 0})
+                result.AppendLine("Empty List");
+        }
+        catch
+        {
+            result.AppendLine("Error reading users list");
+        }
 
         return result.ToString();
     }
